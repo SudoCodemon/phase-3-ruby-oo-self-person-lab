e213c6f57b0ab581a748e4d50c08a815357f0cc7
Daniel Yang <danyang0724@gmail.com>
2022-06-30 17:33:44 -0400

    commit tree a5630b971a848933b27fc2d669eb46c439f2ac28

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,62 @@
 # your code goes here
+class Person
+  attr_reader :name
+  attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def happiness=(value)
+    if value > 10
+      value = 10
+    elsif value < 0
+      value = 0
+    end
+    @happiness = value
+  end
+
+  def hygiene=(value)
+    if value > 10
+      value = 10
+    elsif value < 0
+      value = 0
+    end
+    @hygiene = value
+  end
+  
+  def happy?
+    if self.happiness > 7
+      true
+    else
+      false
+    end
+  end
+
+  def clean?
+    if self.hygiene > 7
+      true
+    else
+      false
+    end
+  end
+
+  def get_paid(amount)
+    self.bank_account += amount
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+end
